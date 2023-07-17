d12fe9f04dc3895a86796c8dcde72ad3b9abc52e
Ruo-Fang Wang <wang0857@algonquinlive.com>
2023-07-17 19:31:18 -0400

    first commit

---
+++ docs/.vuepress/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Hello from the `'HelloVue' component</h2>
+        <h3>{{ $page.title }}</h3>
+    </div>
+</template>
+
+<script>
+    export default{ }
+</script>
+
+<style>
+</style>
---
+++ docs/about.md
@@ -0,0 +1,5 @@
+---
+title: Contact title page
+description: Description of the contact page
+---
+# Here is my contact file
